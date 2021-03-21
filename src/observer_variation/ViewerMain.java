ef5bee4846aca63f718dbc8b8b9a1214095e5d80
mialskywalker <kalqga123@gmail.com>
2021-03-21 19:03:04 +0200

    a

--- src/observer_variation/ViewerMain.java
+++ src/observer_variation/ViewerMain.java
@@ -7,14 +7,10 @@ public class ViewerMain {
         Viewer vie1 = new ExerciseViewer("Yotsuba");
         Viewer vie2 = new ExerciseViewer("Miku");
         Viewer vie3 = new ExerciseViewer("Nino");
-        Viewer vie4 = new ExerciseViewer("Itsuki");
-        Viewer vie5 = new ExerciseViewer("Ichika");
 
         trainer.watch(vie1);
         trainer.watch(vie2);
         trainer.watch(vie3);
-        trainer.watch(vie4);
-        trainer.watch(vie5);
 
         trainer.setExercise("Deadlift");
     }
